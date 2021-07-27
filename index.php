bf9764bf66b14beebaa099aebc2343faf983e72e
weihmayr <weihmayr.m@gmail.com>
2021-07-27 14:05:02 -0300

    Empreendimento destinado ao microempreendedores e páginas de web para servios

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css?v=21" />
+    <title>Document</title>
+</head>
+<body>  
+    <header></header>
+    <main>
+        <div id="content-geral">
+            <section class="sec-custom">
+                <div class="cols-50">
+                    <div id="tarja-button">
+                     <p>
+                       Se você deseja um site simples e objetivo para sue negócio,
+                       clique no botão abaixo para ver as opções que pode colaborar
+                       com seu empreendimento. 
+                     </p>    
+                        <div class="display-btn">
+                        <div id="btn-tarjeta" class="btn" > SEU NEGÓCIO NA WEB </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="cols-50">
+                    
+                </div>
+            </section>
+        </div>
+    </main>
+</body>
+</html>
